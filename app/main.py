d8fa806b38d7e67efa503fb97da49c4f5eced463
PintoJR <guimoraespinto@gmail.com>
2026-09-23 22:01:55 -0300

    Solution01

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+def copy_file(command: str) -> None:
+    arquivo_original = command.split()[1]
+    arquivo_novo = command.split()[2]
+    if arquivo_original == arquivo_novo:
+        return
+
+    with open(arquivo_original, "r"), open(arquivo_novo, "w"):
+        arquivo_novo.write(arquivo_original.read())
