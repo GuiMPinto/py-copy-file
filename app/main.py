012f0b34b461d6d79249d84552148d4b3612a514
PintoJR <guimoraespinto@gmail.com>
2026-09-23 23:09:36 -0300

    Solution098

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def copy_file(command: str) -> None:
             return
 
         try:
-            with open(arquivo_original, "r") as f1, open(
-                arquivo_novo, "w") as f2:
+            with (open(arquivo_original, "r") as f1,
+                  open(arquivo_novo, "w") as f2):
                 f2.write(f1.read())
         except FileNotFoundError:
             return
